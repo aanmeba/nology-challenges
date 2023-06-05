83714d51779deea1d6c7c7ea2d77aae30872aa0e
aanmeba <aanmeba@gmail.com>
2023-06-05 12:41:58 +1000

    add operators challnege

---
+++ js/operators/operators.js
@@ -0,0 +1,21 @@
+console.log("*** Working with ages ***");
+const age = 10;
+const addTen = age + 10;
+console.log(addTen);
+
+const subtractSeven = age - 7;
+console.log(subtractSeven);
+
+const multiplyEleven = age * 11;
+console.log(multiplyEleven);
+
+console.log("*** Calculating Moon Rotations ***");
+const earthDays = 55;
+const daysToRotate = 27.3;
+const numOfRotations = Math.floor(earthDays / daysToRotate);
+console.log(numOfRotations);
+
+console.log("*** Calculating Moon Orbits ***");
+const daysToOrbit = 27.3;
+const numOfOrbits = Math.floor(earthDays / daysToOrbit);
+console.log(numOfOrbits);
