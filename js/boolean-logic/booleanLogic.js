ef13e848b1d5e9f2ffd07e285abda3e4609710b2
aanmeba <aanmeba@gmail.com>
2023-06-06 10:51:31 +1000

    update the solution

--- js/boolean-logic/booleanLogic.js
+++ js/boolean-logic/booleanLogic.js
@@ -5,9 +5,9 @@ console.log(2 === "2"); // false - different type
 console.log("2" + 3 == 23); // true - string concat
 console.log("2" + 3 === 5); // false - string concat
 
-console.log(10 % 3); // true - has value 1
+console.log(10 % 3); // 1
 console.log(10 % 3 === 1); // true
-console.log((100 % 7) % 5); // true - has value 2
+console.log((100 % 7) % 5); // 2
 console.log((100 % 7) % 5 !== 0); // true - has some remainder
 console.log((100 % 7) % 9 == 2); // true - 2%9 = 2
 console.log(!(10 % 2)); // true - !(0)
@@ -16,11 +16,10 @@ console.log(10 % 3 === -1 % 2); // (1 === -1) - false
 console.log((892783 != "89278" + 3) == 0);
 // (892783 != "892783")
 // (false == 0)
-// false - 0 isn't false, it's falsy value?
+// true 💡
 
 console.log(true && false); // false
 console.log(false || true); // true
-console.log(true || false); // true
 console.log(true || false); // true
 console.log(true || false + true); // true
 
@@ -33,8 +32,8 @@ console.log(10 && 123 && -1 && 3); // 3
 console.log(10 && 123 && 0 && 3); // 0 - 0 is falsy value
 console.log((10 && (123 || "") && parseInt("Roisin")) || 23 / 23 - 1);
 // ((10 && 123 && NaN) || -1)
-// (NaN || -1)
-// -1 (truthy)
+// (NaN || 0)
+// 0 (falsy) 💡
 console.log(3 && "Calum" && ("" || 26));
 // (3 && "Calum" && 26)
 // 26
