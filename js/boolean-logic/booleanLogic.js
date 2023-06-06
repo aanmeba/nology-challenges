5f23511e4bf757a45c00d5ac555b62d916ec0b37
aanmeba <aanmeba@gmail.com>
2023-06-06 10:14:42 +1000

    update the explanation

--- js/boolean-logic/booleanLogic.js
+++ js/boolean-logic/booleanLogic.js
@@ -9,11 +9,14 @@ console.log(10 % 3); // true - has value 1
 console.log(10 % 3 === 1); // true
 console.log((100 % 7) % 5); // true - has value 2
 console.log((100 % 7) % 5 !== 0); // true - has some remainder
-console.log((100 % 7) % 9 == 2); // true - 2%9 = 2 ?
+console.log((100 % 7) % 9 == 2); // true - 2%9 = 2
 console.log(!(10 % 2)); // true - !(0)
 console.log(!!!!((10 % 7) % 3)); // !!!!(0) - false
 console.log(10 % 3 === -1 % 2); // (1 === -1) - false
-console.log((892783 != "89278" + 3) == 0); // (892783 != "892783") -> (false == 0) -> false - 0 isn't false, it's falsy value?
+console.log((892783 != "89278" + 3) == 0);
+// (892783 != "892783")
+// (false == 0)
+// false - 0 isn't false, it's falsy value?
 
 console.log(true && false); // false
 console.log(false || true); // true
@@ -22,15 +25,20 @@ console.log(true || false); // true
 console.log(true || false + true); // true
 
 console.log(true * false && false + true);
-// (false && false) -> false ?
 // true - 1 / false - 0
-// 0 && 1 -> 0
+// 0 && 1
+// 0 (falsy)
+
 console.log(10 && 123 && -1 && 3); // 3
 console.log(10 && 123 && 0 && 3); // 0 - 0 is falsy value
 console.log((10 && (123 || "") && parseInt("Roisin")) || 23 / 23 - 1);
-// ((10 && 123 && NaN) || -1) -> (NaN || -1) -> -1
+// ((10 && 123 && NaN) || -1)
+// (NaN || -1)
+// -1 (truthy)
 console.log(3 && "Calum" && ("" || 26));
-// (3 && "Calum" && 26) -> 26
+// (3 && "Calum" && 26)
+// 26
 console.log(3 && "Remi" && (null || 86 * 0 || ("" + 1 && "Cat" + "Dog")));
 // (3 && "Remi" && (null || 0 || "1" && "CatDog"))
-// (3 && "Remi" && ("CatDog")) -> "CatDog"
+// (3 && "Remi" && ("CatDog"))
+// "CatDog"
