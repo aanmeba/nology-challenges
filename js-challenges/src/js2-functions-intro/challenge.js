b456e2884d1286b6c373f73e901b532a4b5293f4
aanmeba <aanmeba@gmail.com>
2023-06-09 14:16:45 +1000

    change a solution

--- js-challenges/src/js2-functions-intro/challenge.js
+++ js-challenges/src/js2-functions-intro/challenge.js
@@ -17,7 +17,7 @@
  * @returns {string} John Smith
  */
 export const createFullName = (firstName, lastName) => {
-  return `${firstName} ${lastName}`;
+  return firstName + " " + lastName;
 };
 
 /**
@@ -29,7 +29,7 @@ export const createFullName = (firstName, lastName) => {
  * @returns {number} 100
  */
 export const findSmallestNumber = (number1, number2) => {
-  return Math.min(number1, number2);
+  return number1 > number2 ? number2 : number1;
 };
 
 /**
