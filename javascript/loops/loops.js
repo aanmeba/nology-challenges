b3e974a4fdc8209198e10da46dad9afabb9f8b11
aanmeba <aanmeba@gmail.com>
2023-06-14 17:42:36 +1000

    remove instructions

--- javascript/loops/loops.js
+++ javascript/loops/loops.js
@@ -1,14 +1,14 @@
-/*** Challenge: Sum of n ***/
-// MVP:
-// Create a function with a for loop that will add all the numbers up to n
-
-// You should create a variable to set the upper limit of your loop
-// Your loop should increment by 1 on each iteration
-// Your loop should print the final result
-
-// ```
-// sum(10) => 55
-// ```
+/*** Challenge: Sum of n
+ * MVP:
+ * Create a function with a for loop that will add all the numbers up to n
+ *
+ * You should create a variable to set the upper limit of your loop
+ * Your loop should increment by 1 on each iteration
+ * Your loop should print the final result
+ * ```
+ * sum(10) => 55
+ * ```
+ * ***/
 
 const sum = (num) => {
   let sum = 0;
@@ -20,27 +20,27 @@ const sum = (num) => {
 
 sum(10);
 
-/*** Challenge: Write a shopping list ***/
-// MVP:
-// Create a function with a for loop that will add numbers to your shopping list
-
-// Store the following array in a variable
-// Your loop should iterate through every value in the array
-// Your loop should print a shopping list with the number and the name of the item
-
-// ```
-// ['milk', 'watermelon', 'eggs', 'soap', 'butter', 'apples', 'ice-cream'];
-// =>
-// // In Console
-
-// 1. milk
-// 2. watermelon
-// 3. eggs
-// 4. soap
-// 5. butter
-// 6. apples
-// 7. ice-cream
-// ```
+/*** Challenge: Write a shopping list
+ * MVP:
+ * Create a function with a for loop that will add numbers to your shopping list
+ *
+ * Store the following array in a variable
+ * Your loop should iterate through every value in the array
+ * Your loop should print a shopping list with the number and the name of the item
+ *
+ * ```
+ * ['milk', 'watermelon', 'eggs', 'soap', 'butter', 'apples', 'ice-cream'];
+ * =>
+ * // In Console
+ * 1. milk
+ * 2. watermelon
+ * 3. eggs
+ * 4. soap
+ * 5. butter
+ * 6. apples
+ * 7. ice-cream
+ * ```
+ * ***/
 
 const shoppingList = [
   "milk",
@@ -60,16 +60,17 @@ const printShoppingList = (arr) => {
 
 printShoppingList(shoppingList);
 
-/*** Challenge: Capitalizing Odd Positioned Letters ***/
-
-// MVP
-// Create a variable with a string of your choice
-// Loop through the letters in this string and build a new string
-// If the current index is odd, capitalize the letter before adding it to the new string
-// Bonus
-// If the current index is even increment the letter
-// E.g: a becomes b, d becomes e, t becomes u
-// Final: z becomes a
+/*** Challenge: Capitalizing Odd Positioned Letters
+ * MVP
+ * Create a variable with a string of your choice
+ * Loop through the letters in this string and build a new string
+ * If the current index is odd, capitalize the letter before adding it to the new string
+ *
+ * Bonus
+ * If the current index is even increment the letter
+ * E.g: a becomes b, d becomes e, t becomes u
+ * Final: z becomes a
+ * ***/
 
 // const string = "hello world";
 const string = "zzzzz";
@@ -92,23 +93,23 @@ for (let i = 0; i < string.length; i++) {
 
 console.log(newString);
 
-/*** Challenge: Removing Vowels ***/
-
-// MVP
-// Create a variable with a string of your choice
-// Loop through the letters in this and build a new string
-// The new strings should be the same as the input with the vowels missing
-
-// ```
-// E.g: calum => clm, rachel => rchl, martyna => mrtyn
-// ```
-
-// Bonus
-// Keep vowels in the new strings if they are succeeded by the letters l, m, or r
-
-// ```
-// E.g: calum => calum, rachel => rchel, martyna => martyn
-// ```
+/*** Challenge: Removing Vowels
+ * MVP
+ * Create a variable with a string of your choice
+ * Loop through the letters in this and build a new string
+ * The new strings should be the same as the input with the vowels missing
+ *
+ * ```
+ * E.g: calum => clm, rachel => rchl, martyna => mrtyn
+ * ```
+ *
+ * Bonus
+ * Keep vowels in the new strings if they are succeeded by the letters l, m, or r
+ *
+ * ```
+ * E.g: calum => calum, rachel => rchel, martyna => martyn
+ * ```
+ * ***/
 
 // const calum = "calum";
 // const rachel = "rachel";
@@ -134,9 +135,9 @@ removeVowels("calum");
 removeVowels("rachel");
 removeVowels("martyna");
 
-/*** Challenge: Loop through an array backwards ***/
-
-// Console.log() every element from the array
+/*** Challenge: Loop through an array backwards
+ * Console.log() every element from the array
+ * ***/
 
 const backwards = [1, 2, 3, 4, 5];
 
--- javascript/operators/operators.js
+++ javascript/operators/operators.js
@@ -1,16 +1,39 @@
-console.log("*** Working with ages ***");
+/*** Challenge: Sum of n
+ * MVP
+ * Create a variable that contains your age
+ * Add 10 to your age variable and log the output to the console
+ * Subtract 7 from your age variable and log the output to the console
+ * Multiple your age variable by 11 and log the output to the console
+ * ***/
+
 let age = 10;
 console.log((age += 10));
 console.log((age -= 7));
 console.log((age *= 11));
 
-console.log("*** Calculating Moon Rotations ***");
+/*** Challenge: Calculating Moon Rotations
+ * MVP
+ * Create two variables
+ * The first for the number of earth days. (You can choose any number of days)
+ * One to hold the number of earth days it takes for the moon to rotate.
+ * Log the number of rotations of the moon given the number of earth days you specified
+ * ***/
+
 const earthDays = 55;
 const daysToRotate = 27.3;
 const numOfRotations = Math.floor(earthDays / daysToRotate);
 console.log(numOfRotations);
 
-console.log("*** Calculating Moon Orbits ***");
+/*** Challenge: Calculating Moon Orbits
+ * MVP
+ * Create another variable to store the number of earth days it takes for the moon to orbit the earth
+ * Log the number of orbits the moon can do, given the number of earth days
+ *
+ * Create a new branch in you nology-challenges
+ * Open a pull request to main once challenge is completed
+ * Review each others pull requests
+ * ***/
+
 const daysToOrbit = 27.3;
 const numOfOrbits = Math.floor(earthDays / daysToOrbit);
 console.log(numOfOrbits);
