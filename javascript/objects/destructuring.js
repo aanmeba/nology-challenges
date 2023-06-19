8c3c9ddee797f18e570470ae1fdccee2ad1bb9da
aanmeba <aanmeba@gmail.com>
2023-06-19 11:38:16 +1000

    reorganise instructions

--- javascript/objects/destructuring.js
+++ javascript/objects/destructuring.js
@@ -6,11 +6,11 @@ const movie = {
   genre: ["Drama", "Teen", "Comedy"],
 };
 
-// Using object destructuring, create a function that returns a string that says what the title and the genres of the passed object are.
-
-// @param {title: string, yearReleased: number, director: string, genres: string[]}
-
-// @returns 'The genres of the movie title are genre1, genre2'
+/**
+ * Using object destructuring, create a function that returns a string that says what the title and the genres of the passed object are.
+ * @param {title: string, yearReleased: number, director: string, genres: string[]}
+ * @returns 'The genres of the movie title are genre1, genre2'
+ */
 
 const getTitleAndGenre = (obj) => {
   const {
@@ -39,10 +39,11 @@ const recipe = {
   ],
 };
 
-// Using object destructuring, create a function that renames the key of an object. It should not modify the original object.
-
-// @param {name: string, country: string, isVegetarian: boolean, mealIngredients: string[]}
-// @returns {name; string, country: string, isVegetarian: boolean, ingredients: string[]}
+/**
+ * Using object destructuring, create a function that renames the key of an object. It should not modify the original object.
+ * @param {name: string, country: string, isVegetarian: boolean, mealIngredients: string[]}
+ * @returns {name; string, country: string, isVegetarian: boolean, ingredients: string[]}
+ */
 
 const renameKey = (obj) => {
   const { mealIngredients, ...rest } = obj;
@@ -68,11 +69,11 @@ const codingSchool = {
   ],
 };
 
-// Using destructuring, create a function that returns the city of the first office of the given company
-
-// @param {name: string, languages: string[], offices: {country: string, city: string}[]}
-
-// @returns {string}  the name of the city of the first office
+/**
+ * Using destructuring, create a function that returns the city of the first office of the given company
+ * @param {name: string, languages: string[], offices: {country: string, city: string}[]}
+ * @returns {string}  the name of the city of the first office
+ */
 
 const returnFirstCity = (obj) => {
   // 1
@@ -115,11 +116,12 @@ const postsArray = [
   },
 ];
 
-// given an array of post objects create a function that returns a new array of posts, each object in the new array should have keys renamed as per the example.
+/**
+ * given an array of post objects create a function that returns a new array of posts, each object in the new array should have keys renamed as per the example.
+ * @param {{id: number, createdBy: number, commentNo: number}[]}
+ * @returns {{postId: number, creator: number, postComment: number}[]}
+ */
 
-// @param {{id: number, createdBy: number, commentNo: number}[]}
-
-// @returns {{postId: number, creator: number, postComment: number}[]}
 const newPostArr = (arr) => {
   return arr.map((obj) => {
     const { id, createdBy, commentNo } = obj;
@@ -130,17 +132,19 @@ const newPostArr = (arr) => {
 console.log(newPostArr(postsArray));
 
 // 5
+
 const forecast = {
   yesterday: { low: 14, high: 32 },
   today: { low: 18, high: 34 },
   tomorrow: { low: 20, high: 28 },
 };
 
-// using object destructuring, create a function that returns the value of the low temperature for today
+/**
+ * using object destructuring, create a function that returns the value of the low temperature for today
+ * @param { yesterday: { low: number, high: number }, today: { low: number, high: number }, tomorrow: { low: number, high: number } }
+ * @returns {number} the value of the low temperature for today
+ */
 
-// @param { yesterday: { low: number, high: number }, today: { low: number, high: number }, tomorrow: { low: number, high: number } }
-
-// return {number} the value of the low temperature for today
 const getTodayLow = (obj) => {
   return obj.today.low;
 };
