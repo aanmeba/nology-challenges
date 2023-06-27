d6545f7d7c225a9f9d125f3854822d6aa4d16f67
aanmeba <aanmeba@gmail.com>
2023-06-27 11:00:08 +1000

    separate banner component and add ratings into stars

---
+++ react/props/react-props-challenge/src/components/Banner/index.jsx
@@ -0,0 +1,11 @@
+import s from "./Banner.module.scss";
+
+const Banner = () => {
+  return (
+    <div className={s.bannerContainer}>
+      <div className={s.banner}>Top Rating</div>
+    </div>
+  );
+};
+
+export default Banner;
--- react/props/react-props-challenge/src/components/Card/index.jsx
+++ react/props/react-props-challenge/src/components/Card/index.jsx
@@ -1,22 +1,28 @@
 // import imgUrl from "../../assets/elemental.jpeg";
+import Banner from "../Banner";
 import s from "./Card.module.scss";
 
 const Card = ({ title, name, rating, imgUrl }) => {
   return (
     <div className={s.container}>
-      {rating == 5 && (
-        <div className={s.bannerContainer}>
-          <div className={s.banner}>Top Rating</div>
-        </div>
-      )}
+      {rating == 5 && <Banner />}
       <img src={imgUrl} alt="" className={s.cover} />
       <div className={s.details}>
         <p className={s.text}>{title}</p>
         <p className={s.text}>- {name}</p>
-        <p className={s.text}>{rating == 5 ? "Top Rating" : rating}</p>
+        {}
+        <p className={s.text}>{ratingStarts(rating)}</p>
       </div>
     </div>
   );
+};
+
+const ratingStarts = (rating) => {
+  let stars = "";
+  for (let i = 0; i < +rating; i++) {
+    stars += "⭐️";
+  }
+  return stars;
 };
 
 export default Card;
