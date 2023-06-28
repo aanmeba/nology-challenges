43e0a8bbaf2c886dfb97adedf69b018ca5c00263
aanmeba <aanmeba@gmail.com>
2023-06-28 14:01:13 +1000

    add bonus challnege

---
+++ react/state/react-state-challnege/README.md
@@ -0,0 +1,22 @@
+## Challenge: Changing mode
+
+### MVP
+
+Inside the App component, create a button that will allow you to toggle between light and dark mode of the page.
+
+- When the mode is set to dark, the background should be dark, all text should be light and the button should say "Change to light mode".
+- When the mode is set to light, the background should be light, all text should be dark and the button should say "Change to dark mode".
+
+### BONUS
+
+Use SVG Icons for the button
+https://www.svgrepo.com/
+
+## Challenge: Generating a random number
+
+### MVP
+
+Inside the App component, add another button that will generate a random number between 0 and 100 when clicked.
+
+- When the user clicks the button display the number on the page.
+- If the generated number is even, render the following message: "The number is even." If its odd, render "The number is odd."
--- react/state/react-state-challnege/src/App.jsx
+++ react/state/react-state-challnege/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import styles from "./App.module.scss";
 import "./App.css";
+import LightMode from "./components/LightMode";
+import DarkMode from "./components/DarkMode";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -25,9 +27,14 @@ function App() {
 
   return (
     <div className={styles.container}>
-      <button className={handleBtnColor} onClick={handleMode}>
+      {isDarkMode ? (
+        <LightMode handleMode={handleMode} />
+      ) : (
+        <DarkMode handleMode={handleMode} />
+      )}
+      {/* <button className={handleBtnColor} onClick={handleMode}>
         {isDarkMode ? "Change to Light Mode" : "Change to Dark Mode"}
-      </button>
+      </button> */}
       <button className={handleBtnColor} onClick={handleNumber}>
         Generate a random number
       </button>
@@ -40,18 +47,3 @@ function App() {
 }
 
 export default App;
-
-// ## Challenge: Changing mode
-// ### MVP
-// Inside the App component, create a button that will allow you to toggle between light and dark mode of the page.
-// -   When the mode is set to dark, the background should be dark, all text should be light and the button should say "Change to light mode".
-// -   When the mode is set to light, the background should be light, all text should be dark and the button should say "Change to dark mode".
-// ### BONUS
-// Use SVG Icons for the button
-// https://www.svgrepo.com/
-
-// ## Challenge: Generating a random number
-// ### MVP
-// Inside the App component, add another button that will generate a random number between 0 and 100 when clicked.
-// -   When the user clicks the button display the number on the page.
-// -   If the generated number is even, render the following message: "The number is even." If its odd, render "The number is odd."
